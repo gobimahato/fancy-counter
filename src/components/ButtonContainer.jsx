c92ba643cffd1b90580955a324f9ed08eb1d5aa2
gobiweb <learngobi@gmail.com>
2025-03-02 01:24:46 +0530

    disable card (count > 5 ) and Limit for pro member

--- src/components/ButtonContainer.jsx
+++ src/components/ButtonContainer.jsx
@@ -1,6 +1,6 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
-export default function ButtonContainer({ setCount }) {
+export default function ButtonContainer({ setCount, locked }) {
   return (
     <div className="button-container">
       <CountButton type="minus" setCount={setCount} />
@@ -9,7 +9,7 @@ export default function ButtonContainer({ setCount }) {
   );
 }
 
-function CountButton({ type, setCount }) {
+function CountButton({ type, setCount, locked }) {
   const handleClick = () => {
     setCount((prev) => {
       if (type === "minus") {
@@ -18,12 +18,18 @@ function CountButton({ type, setCount }) {
         return newCount;
       }
 
-      if (type === "plus") return prev + 1;
+      if (type === "plus") {
+        const newCount = prev + 1;
+        if (newCount > 5) {
+          return 5;
+        }
+        return newCount;
+      }
     });
   };
 
   return (
-    <button onClick={handleClick} className="count-btn">
+    <button disabled={locked} onClick={handleClick} className="count-btn">
       {type === "minus" && <MinusIcon className="count-btn-icon" />}
       {type === "plus" && <PlusIcon className="count-btn-icon" />}
     </button>
