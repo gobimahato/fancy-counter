69c28997e1519d570b86d4455b1d6526b6d6ec61
gobiweb <learngobi@gmail.com>
2025-03-02 00:22:32 +0530

    useState() hook for increase and decrease count

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
 import Count from "./Count";
 import CountButtons from "./CountButtons";
 import ResetButton from "./ResetButton";
 import Title from "./Title";
 
 export default function Card() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="card">
       <Title />
-      <Count number={10} size="lg" />
-      <ResetButton />
-      <CountButtons />
+      <Count count={count} />
+      <ResetButton count={count} />
+      <CountButtons setCount={setCount} />
     </div>
   );
 }
--- src/components/CountButtons.jsx
+++ src/components/CountButtons.jsx
@@ -1,12 +1,16 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
-export default function CountButtons() {
+export default function CountButtons({ count, setCount }) {
   return (
     <div className="button-container">
-      <button className="count-btn">
+      <button
+        onClick={() => setCount((prev) => prev - 1)}
+        className="count-btn">
         <MinusIcon className="count-btn-icon" />
       </button>
-      <button className="count-btn">
+      <button
+        onClick={() => setCount((prev) => prev + 1)}
+        className="count-btn">
         <PlusIcon className="count-btn-icon" />
       </button>
     </div>
